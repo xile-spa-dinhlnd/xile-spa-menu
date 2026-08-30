17d30b485fee4be2f8040d12cac55c0b0d62411b
DinhLuIT <apextaylor00@gmail.com>
2026-08-30 22:50:51 +0700

    fix: cấu hình base path cho vite sửa lỗi 404 github páges

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/xile-spa-menu/', // Cần thiết cho việc deploy lên Github Pages (tên repo)
   plugins: [
     react(),
     tailwindcss(),
